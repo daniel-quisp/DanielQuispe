0dd8738a53200b826241171aa756dee132f8ccb8
daniel-quisp <danielquispehinojosa730@gmail.com>
2026-04-17 08:48:38 -0500

    asdasd

--- js/miprimerJS.js
+++ js/miprimerJS.js
@@ -4,21 +4,22 @@ console.log("Hola mundo desde JavaScript");
 alert("Hola Mundo. Esto es una Alerta");
 
 function cambiarCarita(){
-    document.getElementById("carita").src="images/sad.png";
-     document.getElementById("logoSenati").src="images/senati3.png";
+    document.getElementById("carita").src="imagenes/sad.png";
+     document.getElementById("logoSenati").src="imagenes/jpg.jpg";
     alert("Por que tas triste");
 }
 function feliz(){
-    document.getElementById("carita").src="images/happy.jpg";
+    document.getElementById("carita").src="imagenes/happy.jpg";
     document.getElementById("TituloJS").textContent="Ejemplo de Caritas";
 }
 function nombresNav(){
     document.getElementById("m1").textContent="Index";
-    document.getElementById("m2").textContent="Segunda Página";
-    document.getElementById("m3").textContent="Tercera Página";
-    document.getElementById("m4").textContent="Cuarta Página";
-    document.getElementById("m5").textContent="Quinta Página";
-    document.getElementById("m6").textContent="Sexto Página";
+    document.getElementById("m2").textContent="listas";
+    document.getElementById("m3").textContent="tablas";
+    document.getElementById("m4").textContent="js";
+    document.getElementById("m5").textContent="tarea de js";
+    document.getElementById("m6").textContent="formulario";
+    document.getElementById("m7").textContent="personal";
 }
 
 

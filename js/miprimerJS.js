230c592c27f25163cc04b15870b20948530e8034
daniel-quisp <danielquispehinojosa730@gmail.com>
2026-04-17 09:19:37 -0500

    asasfd

--- js/miprimerJS.js
+++ js/miprimerJS.js
@@ -1,18 +1,20 @@
 // Console.log nos sirve para depurar y probar nuestras salidas
 console.log("Hola mundo desde JavaScript");
-//alert nos sirve para mostrarle un alert al usuario
+
+// alert nos sirve para mostrarle un alert al usuario
 alert("Hola Mundo. Esto es una Alerta");
 
-function cambiarCarita(){
-    document.getElementById("carita").src="imagenes/sad.png";
-     document.getElementById("logoSenati").src="imagenes/jpg.jpg";
+function cambiarCarita() {
+    document.getElementById("carita").src = "imagenes/sad.png";
     alert("Por que tas triste");
 }
-function feliz(){
-    document.getElementById("carita").src="imagenes/happy.jpg";
-    document.getElementById("TituloJS").textContent="Ejemplo de Caritas";
+
+function feliz() {
+    document.getElementById("carita").src = "imagenes/happy.jpg";
+    document.getElementById("TituloJS").textContent = "Ejemplo de Caritas";
 }
-function nombresNav(){
+
+function nombresNav() {
     document.getElementById("m1").textContent = "Index";
     document.getElementById("m2").textContent = "listas";
     document.getElementById("m3").textContent = "tablas";
@@ -20,9 +22,7 @@ function nombresNav(){
     document.getElementById("m5").textContent = "tarea de js";
     document.getElementById("m6").textContent = "formulario";
     document.getElementById("m7").textContent = "personal";
-    
 }
-
 
 
 

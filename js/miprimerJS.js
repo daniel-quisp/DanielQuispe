ff84f8deaaee68000d099bb7d04cb950cf343872
daniel-quisp <danielquispehinojosa730@gmail.com>
2026-04-22 17:09:59 -0500

    Update miprimerJS.js
    ARREGLO

--- js/miprimerJS.js
+++ js/miprimerJS.js
@@ -24,6 +24,47 @@ function nombresNav() {
     document.getElementById("m7").textContent = "personal";
 }
 
+function cambiarTitulo() {
+    document.getElementById("titulo").textContent = "Practicando JS";
+    document.getElementById("titulo").style.color = "#290c60"; 
+    document.getElementById("logoSenati").src = "images/senati3.png";
+    document.getElementById("encabezado").style.background = "#89cce2";
+}
+
+function suma() {
+    let num1 = 7;
+    let num2 = 9;
+    let suma = num1 + num2;
+    alert("La suma es de: " + suma);
+}
+
+function eje01() {
+    let p1 = 10;
+    let p2 = 5;
+    let p3 = 15;
+    let c1 = 3;
+    let c2 = 18;
+    let c3 = 6;
+    let Stotal, total;
+
+    Stotal = (p1 * c1) + (p2 * c2) + (p3 * c3);
+
+    if (Stotal > 100) {
+        total = Stotal * 0.95;
+        alert("Su compra lleva descuento");
+    } else {
+        total = Stotal;
+    }
+    alert("El total a pagar es de: S/." + total);
+}
+
+function eje02() {
+    let amigos = 8;
+    let total = 240;
+    let cuota = total / amigos;
+    alert("Son " + amigos + " amigos y la cuota del total S/." + total + " es de: S/." + cuota);
+}
+
 
 
 

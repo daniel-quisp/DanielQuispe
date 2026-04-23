8d92f1cf162776913b535496d295a30fa99fd99b
daniel-quisp <danielquispehinojosa730@gmail.com>
2026-04-23 17:11:18 -0500

    Update calculadora.js
    formulario corregido

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-
+    //EJEMPLO CALCULADORA
     document.getElementById("formCalculadora").addEventListener("submit", function(e){
         e.preventDefault();// evita que el formulario me vote de la pagina
 
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function(){
         let resultado = document.getElementById("resultado");
 
         if(isNaN(nume1)||isNaN(nume2)){
+            alert("Ingresa un numero pe!");
             resultado.textContent = "Ingrese números válidos";
             return;
         }
@@ -26,18 +27,46 @@ document.addEventListener("DOMContentLoaded", function(){
                 res = nume1*nume2;
                 break;
             case "div":
-                if (nume2 === 0) {
-                 resultado.textContent = "No se puede dividir entre 0";
-                 return;
-            }
-            res = nume1 / nume2;
-            break;
+                alert("haganlo Ustedes");
+                break;
             default:
-            resultado.textContent = "Operación no válida";
-            return;
+                break;
         }
 
     resultado.textContent = "Resultado es "+res;
-    
-    });
 
+    });
+    //ejercicio 04
+   document.getElementById("formEje04").addEventListener("submit", function(e){
+        e.preventDefault();// evita que el formulario me vote de la pagina
+
+        let salario = parseFloat(document.getElementById("salario").value);
+        let horasT = parseFloat(document.getElementById("horasT").value);
+        let horasE=0;
+        let salarioE=0;
+        let sueldo=0;
+        let VariableSalida = document.getElementById("salida");
+      
+        if(isNaN(salario)||isNaN(horasT)){
+            VariableSalida.textContent = "Ingrese números válidos";
+            alert("Ingresa un numero pe!");
+            return;
+        }
+        if(horasT>40){
+            alert("Usted trabajó mas de 40 horas, y merece un pago extra");
+            horasE = horasT-40;
+            salarioE = horasE*(salario*1.5);
+            sueldo = salario*40+salarioE;
+        }else{
+         sueldo = salario*horasT;   
+        }
+
+        VariableSalida.textContent = "Su sueldo a pagar es de S/."+sueldo;
+        });
+     /*   
+        document.getElementById("formEje04").addEventListener("submit", function(e){
+        e.preventDefault();
+            //AQUI ES DONDE RESUELVES TU EJERCICIO
+        });
+*/
+})

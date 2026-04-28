c9a430f8d636a0cbdfa6c3de4d6934fe7bf3077b
daniel-quisp <danielquispehinojosa730@gmail.com>
2026-04-28 15:31:24 -0500

    Update miprimerJS.js

--- js/miprimerJS.js
+++ js/miprimerJS.js
@@ -27,7 +27,7 @@ function nombresNav() {
 function cambiarTitulo() {
     document.getElementById("Ejemplo de JS").textContent = "Practicando JS";
     document.getElementById("titulo").style.color = "#290c60"; 
-    document.getElementById("logoSenati").src = "images/senati3.png";
+    document.getElementById("logoSenati").src = "imagenes/jpg.jpg";
     document.getElementById("encabezado").style.background = "#89cce2";
 }
 
